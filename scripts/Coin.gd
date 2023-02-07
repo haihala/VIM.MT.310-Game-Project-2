f6aef8b8485988cc0d2ef99bb2c007cfa415517c
Eero Häihälä <eero.haihala@gmail.com>
2023-02-07 20:54:53 +0200

    Change coins to animated diamonds

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -2,11 +2,6 @@ extends Area2D
 
 export var value = 1
 
-func _process (delta):
-	# rotate 90 degrees a second
-	rotation_degrees += 90 * delta
-
-
 func _on_Coin_body_entered(body):
 	if body.name == "Player":
 		body.collect_coin(value)
