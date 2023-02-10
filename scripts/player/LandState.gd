b50ee095fba8b2cad306cb47a620c79c9249e4e9
Eero Häihälä <eero.haihala@gmail.com>
2023-02-10 12:53:38 +0200

    Add landing sound, lower footstep volume, speed up walk animation

--- scripts/player/LandState.gd
+++ scripts/player/LandState.gd
@@ -1,9 +1,11 @@
 extends PlayerState
 
 onready var particles = get_node("../../LandingParticles")
+onready var footstep_player = get_node("../../FootstepPlayer")
 
 func enter():
 	.enter()
-
 	if not particles.emitting:
 		particles.restart()
+	footstep_player.play()
+	
--- scripts/player/RunState.gd
+++ scripts/player/RunState.gd
@@ -3,12 +3,15 @@ extends PlayerState
 onready var particles = get_node("../../RunningParticles")
 onready var footstep_player = get_node("../../FootstepPlayer")
 
+
 func enter():
 	.enter()
 	
 	particles.emitting = true
 	footstep_player.play_footsteps = true
+	footstep_player.volume_db = -20
 
 func exit():
 	particles.emitting = false
 	footstep_player.play_footsteps = false
+	footstep_player.volume_db = 0
