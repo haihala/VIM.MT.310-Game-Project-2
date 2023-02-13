45f66061ca96952567ddc5df8b3c48c3c05e5aee
Eero Häihälä <eero.haihala@gmail.com>
2023-02-13 14:50:38 +0200

    First two tutorial levels

--- scripts/level_objects/Crate.gd
+++ scripts/level_objects/Crate.gd
@@ -1,9 +1,15 @@
 extends KinematicBody2D
 
+var vel : Vector2
+
+func _physics_process(delta):
+	vel.y += Constants.gravity * delta
+	vel = move_and_slide(vel, Vector2.UP)
 
 func get_hit():
 	$DestructionParticles.emit()
 	$Sprite.hide()
 	$DestructionSoundPlayer.play_random()
+	$CollisionShape2D.disabled = true
 	yield(get_tree().create_timer(2), "timeout")
 	queue_free() 
