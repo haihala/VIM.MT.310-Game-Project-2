3b8c4cf719eec8d64494442fd2d90ef42a253429
Eero Häihälä <eero.haihala@gmail.com>
2023-02-20 12:49:37 +0200

    Make level for cannon pig

--- scripts/level_objects/CannonBall.gd
+++ scripts/level_objects/CannonBall.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export var speed = 150
+var active = true
 
 func _physics_process(delta):
 	# Because of how the facings work, this has to be -
@@ -8,10 +9,15 @@ func _physics_process(delta):
 
 func _on_Collider_body_entered(body):
 	if body.name == "Player":
-		body.get_hit()
+		hit(body)
 
 func _on_Collider_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
 	if body.name == "TileMap":
 		queue_free()
-	elif body.has_method("get_hit"):
+	else:
+		hit(body)
+
+func hit(body):
+	if body.has_method("get_hit") and active:
 		body.get_hit()
+		active = false
--- scripts/level_objects/CannonPig.gd
+++ scripts/level_objects/CannonPig.gd
@@ -4,7 +4,8 @@ export var cannon_ball : PackedScene
 
 # Idle animation is from a different sized tile sheet
 # This causes the pig to move when it changes animations
-var pig_offset = 5
+var pig_offset = 15
+export var drop : PackedScene
 
 func _ready():
 	var _unused = $Timer.connect("timeout", self, "shoot")
@@ -30,3 +31,20 @@ func fire_ball():
 func spawn_projectile():
 	var instance = cannon_ball.instance()
 	add_child(instance)
+
+func get_hit():
+	$Timer.disconnect("timeout", self, "shoot")
+	$Pig.disconnect("animation_finished", self, "activate_cannon")
+	$Cannon.disconnect("animation_finished", self, "fire_ball")
+	if $Pig.animation == "match":
+		$Pig.position.y -= pig_offset
+	$Pig.play("die")
+	var _unused = $Pig.connect("animation_finished", self, "die")
+
+func die():
+	if drop:
+		var instance = drop.instance()
+		instance.position = $Pig.position
+		add_child(instance)
+	$Pig.queue_free()
+	$CollisionShape2D.disabled = true
